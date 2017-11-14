42ad5a116c7b27911ddf911557d66f837d5a7fe3
Angela <angr.15@hotmail.com>
2017-11-13 19:31:35 -0500

    formulario actualizado

--- public/main.js
+++ public/main.js
@@ -1,9 +1,16 @@
 $(document).ready(function (){
+    $('input[type="submit"]').on("click", function(event) {
+        event.preventDefault();
 
-	$('input[type="submit"]').on("click", function(event) {
-		var first_name = $('input[name="first_name"]').val();
-		console.log(first_name);
+        var first_name = $('#first_name').val();
 
-		event.preventDefault();
-	});
-})
+       /* $.ajax('find.html' , { 
+        	method: 'post',
+        	data: {
+        		'first_name': first_name
+        	}
+    	});*/
+    	$.get('find.html' ,  { 'first_name': first_name});
+    	$.post('find.html' ,  { 'first_name': first_name});
+    });
+});
