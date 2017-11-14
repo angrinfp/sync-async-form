b1bc1d4163ebfa269fd62dc5d04b68e07fa05de5
Alumno11 J601 <a11j601@campustls.local>
2017-11-14 12:10:50 -0500

    ajax

--- public/main.js
+++ public/main.js
@@ -1,16 +1,41 @@
 $(document).ready(function (){
-    $('input[type="submit"]').on("click", function(event) {
-        event.preventDefault();
+   $('input[type="submit"]').on("click", function(event) {
+       event.preventDefault();
 
-        var first_name = $('#first_name').val();
+      /* $.ajax('find.html' , {
+           method: 'post',
+           data: {
+               'first_name': first_name
+           }
+       });*/
 
-       /* $.ajax('find.html' , { 
-        	method: 'post',
-        	data: {
-        		'first_name': first_name
-        	}
-    	});*/
-    	$.get('find.html' ,  { 'first_name': first_name});
-    	$.post('find.html' ,  { 'first_name': first_name});
-    });
+    var first_name = $('#first_name').val(); // [first_name]
+
+    var last_name = $('#last_name').val(); // [last_name]
+
+    var gender = $('input[name="gender"]:checked').val(); // []
+
+    var interest = $('input[name="interest"]:checked'); // [1, 2]
+    var interests = new Array();
+
+    // input[name="interest"]:checked -> un selector
+    // $('input[name="interest"]:checked'); -> array de coincidencias
+    // .val(); -> saca el valor de la primera coincidencia, osea el primero chequeado
+
+    for (var i = 0; i < interest.length; i++) {
+        var temp_interest = interest[i];
+        interests.push(temp_interest.value);
+    }
+
+       var payload = {
+        'first_name': first_name,
+        'last_name': last_name,
+        'gender': gender,
+        'interests': interests
+       }
+
+       $.post('find.html', payload);
+       $.get('find.html' , payload);
+
+   });
 });
